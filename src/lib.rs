2633ceb8e8b3f52ccaeef84e897a13f14fb4eb83
Ingvar Stepanyan <me@rreverser.com>
2017-11-28 22:22:38 +0000

    Add parse_string, make parse_* methods public

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate nom;
 
 use nom::*;
 use std::str::FromStr;
+use std::borrow::Cow;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Operator {
@@ -23,13 +24,13 @@ pub enum IdentifierLike<'a> {
     Operator(Operator),
 }
 
-named!(parse_unsigned(&str) -> u64, alt!(
+named!(pub parse_unsigned(&str) -> u64, alt!(
     preceded!(tag!("0x"), map_res!(hex_digit, |digits| u64::from_str_radix(digits, 16))) |
     preceded!(tag!("0"), map_res!(oct_digit, |digits| u64::from_str_radix(digits, 8))) |
     map_res!(digit, u64::from_str)
 ));
 
-named!(parse_operator(&str) -> Operator, alt!(
+named!(pub parse_operator(&str) -> Operator, alt!(
     value!(Operator::Equal, tag!("==")) |
     value!(Operator::NotEqual, tag!("!=")) |
     value!(Operator::GreaterThanEqual, tag!(">=")) |
@@ -40,7 +41,7 @@ named!(parse_operator(&str) -> Operator, alt!(
     value!(Operator::BitwiseAnd, tag!("&"))
 ));
 
-named!(parse_identifier_like(&str) -> IdentifierLike, switch!(alpha,
+named!(pub parse_identifier_like(&str) -> IdentifierLike, switch!(alpha,
     "eq" => value!(IdentifierLike::Operator(Operator::Equal)) |
     "ne" => value!(IdentifierLike::Operator(Operator::NotEqual)) |
     "gt" => value!(IdentifierLike::Operator(Operator::GreaterThan)) |
@@ -64,7 +65,7 @@ named!(hex_byte_pair(&str) -> [u8; 2], do_parse!(
     ([b1, b2])
 ));
 
-named!(parse_ethernet_addr(&str) -> [u8; 6], do_parse!(
+named!(pub parse_ethernet_addr(&str) -> [u8; 6], do_parse!(
     w1: hex_byte_pair >>
     ethernet_separator >>
     w2: hex_byte_pair >>
@@ -75,7 +76,7 @@ named!(parse_ethernet_addr(&str) -> [u8; 6], do_parse!(
 
 named!(dec_byte(&str) -> u8, map_res!(digit, u8::from_str));
 
-named!(parse_ipv4(&str) -> [u8; 4], do_parse!(
+named!(pub parse_ipv4(&str) -> [u8; 4], do_parse!(
     b1: dec_byte >>
     char!('.') >>
     b2: dec_byte >>
@@ -84,6 +85,28 @@ named!(parse_ipv4(&str) -> [u8; 4], do_parse!(
     char!('.') >>
     b4: dec_byte >>
     ([b1, b2, b3, b4])
+));
+
+named!(oct_byte(&str) -> u8, map_res!(take!(3), |digits| u8::from_str_radix(digits, 8)));
+
+named!(pub parse_string(&str) -> Cow<str>, do_parse!(
+    char!('"') >>
+    unprefixed: map!(is_not!("\"\\"), Cow::Borrowed) >>
+    res: fold_many0!(preceded!(char!('\\'), tuple!(
+        alt!(
+            preceded!(char!('x'), map!(hex_byte, |b| b as char)) |
+            map!(oct_byte, |b| b as char) |
+            anychar
+        ),
+        map!(opt!(is_not!("\"\\")), Option::unwrap_or_default)
+    )), unprefixed, |acc: Cow<str>, (ch, rest)| {
+        let mut acc = acc.into_owned();
+        acc.push(ch);
+        acc.push_str(rest);
+        Cow::Owned(acc)
+    }) >>
+    char!('"') >>
+    (res)
 ));
 
 #[cfg(test)]
@@ -169,6 +192,22 @@ mod tests {
         assert_eq!(
             parse_ipv4("12.34.56.789"),
             IResult::Error(error_position!(ErrorKind::MapRes, "789"))
+        );
+    }
+
+    #[test]
+    fn test_string() {
+        assert_eq!(
+            parse_string(r#""hello, world";"#),
+            IResult::Done(";", Cow::Borrowed("hello, world"))
+        );
+        assert_eq!(
+            parse_string(r#""esca\x0a\ped\042";"#),
+            IResult::Done(";", Cow::Owned(String::from("esca\nped\"")))
+        );
+        assert_eq!(
+            parse_string("\"hello"),
+            IResult::Incomplete(Needed::Size(7))
         );
     }
 }
