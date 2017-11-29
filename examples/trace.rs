7148ea866ab7aecfeccf33953adf8d5fc82a1867
Ingvar Stepanyan <me@rreverser.com>
2017-11-29 17:24:57 +0000

    Print IResult in tracer

--- examples/trace.rs
+++ examples/trace.rs
@@ -5,5 +5,5 @@ use wirefilter::{Parse, Filter};
 
 fn main() {
     let s = args().nth(1).expect("Expected a filter as a command-line argument");
-    println!("{:#?}", Filter::parse(&s).unwrap().1);
+    println!("{:#?}", Filter::parse(&s));
 }
