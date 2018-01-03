0f19807d5ae87a0fd1073e69ba8c0c2384810772
Ingvar Stepanyan <me@rreverser.com>
2018-01-03 18:24:55 +0000

    Add Field::lex tests

--- src/field.rs
+++ src/field.rs
@@ -37,3 +37,32 @@ impl<'a> Lex<'a> for Field<'a> {
         Ok((Field::new(span(initial_input, input)), input))
     }
 }
+
+#[test]
+fn test() {
+    use super::ErrorKind;
+
+    assert_ok!(
+        Field::lex("x;"),
+        Field::new("x"),
+        ";"
+    );
+
+    assert_ok!(
+        Field::lex("x.y.z-"),
+        Field::new("x.y.z"),
+        "-"
+    );
+
+    assert_err!(
+        Field::lex("x..y"),
+        ErrorKind::CountMismatch("alphabetic character", 0, 1),
+        ".y"
+    );
+
+    assert_err!(
+        Field::lex("x.0"),
+        ErrorKind::CountMismatch("alphabetic character", 0, 1),
+        "0"
+    );
+}
