116b03a95f3527208e191f966ae766bda60a5148
Ivan Nikulin <inikulin@cloudflare.com>
2018-02-02 10:22:11 +0000

    Fix parsing of decimal zeros

--- engine/src/number.rs
+++ engine/src/number.rs
@@ -12,10 +12,12 @@ fn number(input: &str, radix: u32) -> LexResult<u64> {
 
 impl<'i> Lex<'i> for u64 {
     fn lex(input: &str) -> LexResult<u64> {
-        if let Ok(input) = expect(input, "0x") {
-            number(input, 16)
-        } else if let Ok(input) = expect(input, "0") {
-            number(input, 8)
+        if let Ok(input) = expect(input, "0") {
+            match input.chars().next() {
+                Some(c) if c.is_digit(8) => number(input, 8),
+                Some('x') => number(&input[1..], 16),
+                _ => Ok((0, input))
+            }
         } else {
             number(input, 10)
         }
@@ -81,4 +83,14 @@ impl<'i> Lex<'i> for Vec<Range> {
             }
         }
     }
+}
+
+#[test]
+fn test() {
+    assert_ok!(u64::lex("0"), 0u64, "");
+    assert_ok!(u64::lex("0-"), 0u64, "-");
+    assert_ok!(u64::lex("0x1f5+"), 501u64, "+");
+    assert_ok!(u64::lex("0123;"), 83u64, ";");
+    assert_ok!(u64::lex("78!"), 78u64, "!");
+    assert_ok!(u64::lex("0xefg"), 239u64, "g");
 }
