6c5697f22d321c6843734a8953bcfb5c9d3d8a41
Ingvar Stepanyan <me@rreverser.com>
2018-02-14 14:49:39 +0000

    EGS-341: Add building of Debian package

--- cfsetup-cargo.sh
+++ cfsetup-cargo.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
 export CARGO_HOME=/var/lib/cargo
 export CARGO_TARGET_DIR=$CARGO_HOME/target
@@ -7,21 +6,27 @@ export CARGO_TARGET_DIR=$CARGO_HOME/target
 CMD=$1
 shift
 
-if [ $CMD = prebuild ]
-then
-	# Series of hacks to prebuild dependencies in a cached layer
-	# (workaround for https://github.com/rust-lang/cargo/issues/2644)
+PARAMS="-q $@"
 
-	# Create dummy sources for our library
-	mkdir {engine,ffi}/src
-	touch {engine,ffi}/src/lib.rs
+set -x
 
-	# Build library with Cargo.lock (including all the dependencies)
-	cargo build -q --locked --all "$@"
+case $CMD in
+	prebuild)
+		# Series of hacks to prebuild dependencies in a cached layer
+		# (workaround for https://github.com/rust-lang/cargo/issues/2644)
 
-	# Clean artifacts of the library itself but keep prebuilt deps
-	cargo clean -p wirefilter-engine -p wirefilter-ffi "$@"
-else
-	# Execute any other command assuming that any deps are already in cache
-	cargo $CMD --frozen "$@"
-fi
+		# Create dummy sources for our library
+		mkdir {engine,ffi}/src
+		touch {engine,ffi}/src/lib.rs
+
+		# Build library with Cargo.lock (including all the dependencies)
+		cargo build --locked --all $PARAMS
+
+		# Clean artifacts of the library itself but keep prebuilt deps
+		cargo clean --locked -p wirefilter-engine -p wirefilter-ffi $PARAMS
+		;;
+	*)
+		# Execute any other command without special params but in same env
+		cargo $CMD $PARAMS
+		;;
+esac
