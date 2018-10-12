d0a46d6f5fac5526ee301e7c1352fe8c156b997b
Ingvar Stepanyan <me@rreverser.com>
2018-10-12 14:26:16 +0100

    Use binary search in RangeSet
    Now that it's not exposed in AST, we can optimise it in whichever way we want.
    
    After this change, range sets are going to be presorted during compilation and use binary search during execution.
    
    This brings execution complexity to O(ln N) instead of O(N), which slows down "best" edge cases where IP range previously happened to be one of the first, but instead significantly (2x) speeds up average case with arbitrary lookup, especially on large sets.

--- engine/src/range_set.rs
+++ engine/src/range_set.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Borrow, iter::FromIterator, ops::RangeInclusive};
+use std::{borrow::Borrow, cmp::Ordering, iter::FromIterator, ops::RangeInclusive};
 
 // RangeSet provides a set-like interface that allows to search for items while
 // being constructed from and storing inclusive ranges in a compact fashion.
@@ -6,13 +6,28 @@ pub struct RangeSet<T> {
     ranges: Vec<RangeInclusive<T>>,
 }
 
-impl<T> From<Vec<RangeInclusive<T>>> for RangeSet<T> {
-    fn from(ranges: Vec<RangeInclusive<T>>) -> Self {
+impl<T: Ord + Copy> From<Vec<RangeInclusive<T>>> for RangeSet<T> {
+    // Convert from vector and prepare for binary search.
+    fn from(mut ranges: Vec<RangeInclusive<T>>) -> Self {
+        ranges.sort_unstable_by_key(|range| *range.start());
+        // `dedup_by` invokes callback with mutable references, which allows not only
+        // to remove same consecutive items, but also to provide custom merging
+        // implementation which is exactly what we want for overlapping ranges.
+        ranges.dedup_by(|b, a| {
+            if b.start() <= a.end() {
+                if b.end() > a.end() {
+                    *a = *a.start()..=*b.end();
+                }
+                true
+            } else {
+                false
+            }
+        });
         RangeSet { ranges }
     }
 }
 
-impl<T> FromIterator<RangeInclusive<T>> for RangeSet<T> {
+impl<T: Ord + Copy> FromIterator<RangeInclusive<T>> for RangeSet<T> {
     fn from_iter<I: IntoIterator<Item = RangeInclusive<T>>>(ranges: I) -> Self {
         Vec::from_iter(ranges).into()
     }
@@ -25,7 +40,14 @@ impl<T> RangeSet<T> {
         Q: ?Sized + Ord,
     {
         self.ranges
-            .iter()
-            .any(|range| range.start().borrow() <= value && value <= range.end().borrow())
+            .binary_search_by(|range| {
+                if range.start().borrow() > value {
+                    Ordering::Greater
+                } else if range.end().borrow() >= value {
+                    Ordering::Equal
+                } else {
+                    Ordering::Less
+                }
+            }).is_ok()
     }
 }
