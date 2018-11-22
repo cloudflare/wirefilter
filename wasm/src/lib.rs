59141c4c67a75e63f6ccfb12624a7a27f97ba5f0
Ingvar Stepanyan <me@rreverser.com>
2018-11-22 14:44:42 +0000

    Separate Scheme creation and parsing in WASM

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,12 +1,26 @@
 extern crate wasm_bindgen;
 extern crate wirefilter;
 
+use std::error::Error;
 use wasm_bindgen::prelude::*;
-use wirefilter::Scheme;
 
 #[wasm_bindgen]
-pub fn parse_filter(fields: &JsValue, s: &str) -> JsValue {
-    let scheme: Scheme = fields.into_serde().unwrap();
-    let filter = scheme.parse(s).unwrap();
-    JsValue::from_serde(&filter).unwrap()
+pub struct Scheme(wirefilter::Scheme);
+
+#[cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
+fn into_js_error(err: impl Error) -> JsValue {
+    err.to_string().into()
+}
+
+#[wasm_bindgen]
+impl Scheme {
+    #[wasm_bindgen(constructor)]
+    pub fn new(fields: &JsValue) -> Result<Scheme, JsValue> {
+        fields.into_serde().map(Scheme).map_err(into_js_error)
+    }
+
+    pub fn parse(&self, s: &str) -> Result<JsValue, JsValue> {
+        let filter = self.0.parse(s).map_err(into_js_error)?;
+        JsValue::from_serde(&filter).map_err(into_js_error)
+    }
 }
