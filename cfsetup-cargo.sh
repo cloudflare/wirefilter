b89de9ec48a52c5bf78b6ec7e2473cf0c2659988
Ingvar Stepanyan <me@rreverser.com>
2018-11-30 12:39:16 +0000

    EGS-1186: Add WASM targets to cfsetup
    Together with already merged teamcity-plans counterpart, this allows building, packaging and publishing WASM target of Wirefilter to our NPM directly from CI.

--- cfsetup-cargo.sh
+++ cfsetup-cargo.sh
@@ -26,6 +26,20 @@ case $CMD in
 		# Clean artifacts of the library itself but keep prebuilt deps
 		cargo clean --locked -p wirefilter-engine -p wirefilter-ffi -p wirefilter-wasm $@
 		;;
+	wasm-pack)
+		# Latest release of wasm-pack can't find target via CARGO_TARGET_DIR nor
+		# in a workspace root.
+		#
+		# This is fixed on git master, but we'd rather not build tools during
+		# CI build, so let's hack around this limitation for now by creating
+		# a temporary symlink and cleaning it up afterwards.
+		#
+		# TODO: remove following two commands on next wasm-pack upgrade.
+		ln -s $CARGO_TARGET_DIR $1/target
+		trap "rm $1/target" EXIT
+		wasm-pack build $@
+		wasm-pack pack $1
+		;;
 	*)
 		# Execute any other command without special params but in same env
 		cargo $CMD $@
