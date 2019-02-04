d2c84f67fc63486a9b79cf21868b5d7a2d7958eb
Ivan Nikulin <inikulin@cloudflare.com>
2019-02-04 16:39:39 +0000

    Move CompileExpr to the separate module. Make CompiledExpr::new() invisible outside the crate

--- engine/src/ast/mod.rs
+++ engine/src/ast/mod.rs
@@ -3,40 +3,11 @@ mod field_expr;
 mod simple_expr;
 
 use self::combined_expr::CombinedExpr;
-use execution_context::ExecutionContext;
+use compiled_expr::CompiledExpr;
 use lex::{LexResult, LexWith};
 use scheme::{Field, Scheme, UnknownFieldError};
 use serde::Serialize;
 use std::fmt::{self, Debug};
-
-/// An IR for a compiled filter expression.
-///
-/// Currently it works by creating and combining boxed untyped closures and
-/// performing indirect calls between them, which is fairly cheap, but,
-/// surely, not as fast as an inline code with real JIT compilers.
-///
-/// On the other hand, it's much less risky than allocating, trusting and
-/// executing code at runtime, because all the code being executed is
-/// already statically generated and verified by the Rust compiler and only the
-/// data differs. For the same reason, our "compilation" times are much better
-/// than with a full JIT compiler as well.
-///
-/// In the future the underlying representation might change, but for now it
-/// provides the best trade-off between safety and performance of compilation
-/// and execution.
-pub struct CompiledExpr<'s>(Box<dyn 's + Fn(&ExecutionContext<'s>) -> bool>);
-
-impl<'s> CompiledExpr<'s> {
-    /// Creates a compiled expression IR from a generic closure.
-    pub fn new(closure: impl 's + Fn(&ExecutionContext<'s>) -> bool) -> Self {
-        CompiledExpr(Box::new(closure))
-    }
-
-    /// Executes a filter against a provided context with values.
-    pub fn execute(&self, ctx: &ExecutionContext<'s>) -> bool {
-        self.0(ctx)
-    }
-}
 
 trait Expr<'s>: Sized + Eq + Debug + for<'i> LexWith<'i, &'s Scheme> + Serialize {
     fn uses(&self, field: Field<'s>) -> bool;
---
+++ engine/src/compiled_expr.rs
@@ -0,0 +1,30 @@
+use execution_context::ExecutionContext;
+
+/// An IR for a compiled filter expression.
+///
+/// Currently it works by creating and combining boxed untyped closures and
+/// performing indirect calls between them, which is fairly cheap, but,
+/// surely, not as fast as an inline code with real JIT compilers.
+///
+/// On the other hand, it's much less risky than allocating, trusting and
+/// executing code at runtime, because all the code being executed is
+/// already statically generated and verified by the Rust compiler and only the
+/// data differs. For the same reason, our "compilation" times are much better
+/// than with a full JIT compiler as well.
+///
+/// In the future the underlying representation might change, but for now it
+/// provides the best trade-off between safety and performance of compilation
+/// and execution.
+pub struct CompiledExpr<'s>(Box<dyn 's + Fn(&ExecutionContext<'s>) -> bool>);
+
+impl<'s> CompiledExpr<'s> {
+    /// Creates a compiled expression IR from a generic closure.
+    pub(crate) fn new(closure: impl 's + Fn(&ExecutionContext<'s>) -> bool) -> Self {
+        CompiledExpr(Box::new(closure))
+    }
+
+    /// Executes a filter against a provided context with values.
+    pub fn execute(&self, ctx: &ExecutionContext<'s>) -> bool {
+        self.0(ctx)
+    }
+}
