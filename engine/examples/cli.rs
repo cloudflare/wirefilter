b3a62b4b2a945787d10e8c37328f6728defc89d9
Elie ROUDNINSKI <elie@cloudflare.com>
2019-03-12 23:15:18 +0000

    Add dummy panic function to cli example

--- engine/examples/cli.rs
+++ engine/examples/cli.rs
@@ -1,19 +1,38 @@
 extern crate wirefilter;
 
 use std::env::args;
-use wirefilter::Scheme;
+use wirefilter::{Function, FunctionArg, FunctionArgKind, FunctionImpl, LhsValue, Scheme, Type};
+
+fn panic_function<'a>(_: &[LhsValue<'a>]) -> LhsValue<'a> {
+    panic!();
+}
 
 fn main() {
     let filter = args()
         .nth(1)
         .expect("Expected an input as a command-line argument");
 
-    let scheme = Scheme! {
+    let mut scheme = Scheme! {
         ip: Ip,
         str: Bytes,
         int: Int,
         bool: Bool,
     };
+
+    scheme
+        .add_function(
+            "panic".into(),
+            Function {
+                args: vec![FunctionArg {
+                    arg_kind: FunctionArgKind::Field,
+                    val_type: Type::Bytes,
+                }],
+                opts: vec![],
+                return_type: Type::Bytes,
+                implementation: FunctionImpl::new(panic_function),
+            },
+        )
+        .unwrap();
 
     match scheme.parse(&filter) {
         Ok(res) => println!("{:#?}", res),
