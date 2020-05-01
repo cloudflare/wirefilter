e7194dbb42d7b4659825c89bea1ede9c1aa39874
Ivan Nikulin <inikulin@cloudflare.com>
2020-05-01 21:32:55 +0100

    V2 parser: implement int literal parsing

--- wirefilter-parser/src/lib.rs
+++ wirefilter-parser/src/lib.rs
@@ -1,25 +1,51 @@
 pub mod ast;
 
-use pest_consume::{match_nodes, Error, Parser as PestParser};
+use pest::error::ErrorVariant;
+use pest_consume::{match_nodes, Error as ParseError, Parser as PestParser};
+use std::error::Error;
+use std::fmt::Display;
 
 #[derive(PestParser)]
 #[grammar = "./grammar.pest"]
 pub struct Parser;
 
-pub type ParseResult<T> = Result<T, Error<Rule>>;
+pub type ParseResult<T> = Result<T, ParseError<Rule>>;
 pub type Node<'i> = pest_consume::Node<'i, Rule, ()>;
+
+trait IntoParseResult<T> {
+    fn into_parse_result(self, node: Node) -> ParseResult<T>;
+}
+
+impl<T, E> IntoParseResult<T> for Result<T, E>
+where
+    E: Error + Display,
+{
+    fn into_parse_result(self, node: Node) -> ParseResult<T> {
+        self.map_err(|e| {
+            let span = node.as_span();
+
+            let err_var = ErrorVariant::CustomError {
+                message: e.to_string(),
+            };
+
+            ParseError::new_from_span(err_var, span)
+        })
+    }
+}
 
 #[pest_consume::parser]
 impl Parser {
-    fn variable_name(node: Node) -> ParseResult<ast::Var> {
+    fn var(node: Node) -> ParseResult<ast::Var> {
         // TODO check in scheme
         Ok(ast::Var(node.as_str()))
     }
 
-    fn int_literal(node: Node) -> ParseResult<ast::Rhs> {
+    fn int_lit(node: Node) -> ParseResult<ast::Rhs> {
         let mut num = match_nodes! {
             node.children();
-            [dec_digits(d)] => d
+            [dec_digits(d)] => d,
+            [oct_digits(d)] => d,
+            [hex_digits(d)] => d
         };
 
         if let Some('-') = node.as_str().chars().next() {
@@ -29,8 +55,19 @@ impl Parser {
         Ok(ast::Rhs::Int(num))
     }
 
+    #[inline]
     fn dec_digits(node: Node) -> ParseResult<i32> {
-        Ok(32)
+        i32::from_str_radix(node.as_str(), 10).into_parse_result(node)
+    }
+
+    #[inline]
+    fn oct_digits(node: Node) -> ParseResult<i32> {
+        i32::from_str_radix(node.as_str(), 8).into_parse_result(node)
+    }
+
+    #[inline]
+    fn hex_digits(node: Node) -> ParseResult<i32> {
+        i32::from_str_radix(node.as_str(), 16).into_parse_result(node)
     }
 }
 
@@ -47,26 +84,23 @@ mod tests {
     }
 
     #[test]
-    fn parse_variable_name() {
-        assert_eq!(parse!(variable_name, "foo"), Ok(ast::Var("foo")));
-        assert_eq!(parse!(variable_name, "f1o2o3"), Ok(ast::Var("f1o2o3")));
-
-        assert_eq!(
-            parse!(variable_name, "f1o2o3.bar321"),
-            Ok(ast::Var("f1o2o3.bar321"))
-        );
-
-        assert_eq!(
-            parse!(variable_name, "foo.bar.baz"),
-            Ok(ast::Var("foo.bar.baz"))
-        );
-
-        assert!(parse!(variable_name, "123foo").is_err());
+    fn parse_var() {
+        assert_eq!(parse!(var, "foo"), Ok(ast::Var("foo")));
+        assert_eq!(parse!(var, "f1o2o3"), Ok(ast::Var("f1o2o3")));
+        assert_eq!(parse!(var, "f1o2o3.bar321"), Ok(ast::Var("f1o2o3.bar321")));
+        assert_eq!(parse!(var, "foo.bar.baz"), Ok(ast::Var("foo.bar.baz")));
+        assert!(parse!(var, "123foo").is_err());
     }
 
     #[test]
-    fn parse_int_literal() {
-        assert_eq!(parse!(int_literal, "42"), Ok(ast::Rhs::Int(42)));
-        assert_eq!(parse!(int_literal, "-42"), Ok(ast::Rhs::Int(-42)));
+    fn parse_int_lit() {
+        assert_eq!(parse!(int_lit, "42"), Ok(ast::Rhs::Int(42)));
+        assert_eq!(parse!(int_lit, "-42"), Ok(ast::Rhs::Int(-42)));
+        assert_eq!(parse!(int_lit, "0x2A"), Ok(ast::Rhs::Int(42)));
+        assert_eq!(parse!(int_lit, "-0x2a"), Ok(ast::Rhs::Int(-42)));
+        assert_eq!(parse!(int_lit, "052"), Ok(ast::Rhs::Int(42)));
+        assert_eq!(parse!(int_lit, "-052"), Ok(ast::Rhs::Int(-42)));
+        assert!(parse!(int_lit, "-abc").is_err());
+        assert!(parse!(int_lit, "99999999999999999999999999999").is_err());
     }
 }
